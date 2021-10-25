aade4533ebb9bdf9c73d1a0482bab589fa28d231
SantiagoMS <santiagomusa@unisabana.edu.co>
2021-10-24 22:48:48 -0500

    modify the model

--- src/main/java/com/sabanahabbo/meetu/repository/EventRepository.java
+++ src/main/java/com/sabanahabbo/meetu/repository/EventRepository.java
@@ -16,7 +16,7 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     @Query(value = "select ev.id, date,interest,message,place,time,gr.title from events ev full outer join groups gr on ev.group_id= gr.id", nativeQuery = true)
     List <Map<String, Object>> getAllEvents();
 
-    @Query(value = "select e.id, date, interest, message, place, time from events e INNER JOIN interests int on int.name = e.interest INNER JOIN student_interest stint on stint.interest_id = int.id INNER JOIN students st on stint.student_id = st.id where e.interest in (:PARAM)", nativeQuery = true)
+    @Query(value = "select e.id, date, interest, message, place, time from events e INNER JOIN interests int on int.name = e.interest where e.interest in =:PARAM", nativeQuery = true)
     List <Map<String, Object>> getAllEventsByInterest(@Param("PARAM") List<String> PARAM);
 
     @Query(value = "select e.id, date, message, place, time, gr.title from events e INNER JOIN groups gr on gr.id = e.group_id where e.group_id in (:PARAM)", nativeQuery = true)
